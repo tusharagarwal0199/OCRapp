96b04fef3c3cec50e899d0a9c6531bb4190dfbd6
tusharagarwal0199 <37259130+tusharagarwal0199@users.noreply.github.com>
2019-08-25 23:15:42 +0530

    uploaded complete backend

---
+++ OCR-backend.java
@@ -0,0 +1,151 @@
+package com.example.tushar.ocr1;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    SurfaceView surfaceView;
+    TextView textView;
+    CameraSource cameraSource;
+    final int CameraPermissionId = 1001;
+    Button button2,button3;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CameraPermissionId: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                        return;
+                }
+                try {
+                    cameraSource.start(surfaceView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button2 = (Button) findViewById(R.id.button2);
+        surfaceView = (SurfaceView) findViewById(R.id.sv);
+        textView = (TextView) findViewById(R.id.tv);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = v.toString();
+                if (s.equalsIgnoreCase("stop")) {
+                    cameraSource.stop();
+                    button2.setText("START");
+                } else {
+                    button2.setText("STOP");
+                    TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+                    if (!textRecognizer.isOperational()) {
+                        Log.w("MainActivity", "DD not av");
+                    } else {
+                        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                                .setFacing(CameraSource.CAMERA_FACING_BACK).setRequestedPreviewSize(1280, 1024)
+                                .setRequestedFps(2.0f)
+                                .setAutoFocusEnabled(true)
+                                .build();
+                        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                            @Override
+                            public void surfaceCreated(SurfaceHolder holder) {
+                                try {
+                                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, CameraPermissionId);
+                                        return;
+                                    }
+                                    cameraSource.start(surfaceView.getHolder());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+                            }
+
+                            @Override
+                            public void surfaceDestroyed(SurfaceHolder holder) {
+                                cameraSource.stop();
+                            }
+                        });
+
+                        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                            @Override
+                            public void release() {
+
+                            }
+
+                            @Override
+                            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                                final SparseArray<TextBlock> items = detections.getDetectedItems();
+                                if (items.size() != 0)
+                                {
+                                    textView.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            StringBuilder sb= new StringBuilder();
+                                            for(int i=0;i<items.size();i++)
+                                            {
+                                                TextBlock item = items.valueAt(i);
+                                                sb.append(item.getValue()+"\n");
+                                            }
+                                            textView.setText(sb.toString());
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+
+            }
+        });
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String a = textView.getText().toString();
+                String s1="";
+                for(int i=0;i<a.length();i++)
+                {
+                    char c = a.charAt(i);
+                    if (c==' ')
+                        s1=s1+"+";
+                    else s1=s1+c;
+                }
+                Intent ib= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com/search?q="+s1));
+                startActivity(ib);
+            }
+        });
+    }
+}
